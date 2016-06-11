d03c6acc5fc44b2939900a57eaeaee220228808b
HackeZ <767110505@qq.com>
2016-06-11 10:58:36 +0800

    fix 1.4 wrong

--- Part 1 String/1.4 StrToInt/main.go
+++ Part 1 String/1.4 StrToInt/main.go
@@ -1,16 +1,54 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
+func main() {
+	// strconv.Atoi("123")
+	n, err := StrToInt("-8 19")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("StrToInt --> ", n)
+	}
 }
 
 // Author: HackerZ
 // Time: 2016/06/10
 
 // StrToInt
-// Input: "123"
-// Output: 123
+// Input: "123" ==> string
+// Output: 123  ==> int
 
-func StrToInt(str string) int {
-	return 1
+// StrToInt foce on detail
+func StrToInt(str string) (int, error) {
+	// is nil
+	if len(str) == 0 {
+		return 0, errors.New("Error: Type of var.")
+	}
+
+	// Handle Space
+	str = strings.Replace(str, " ", "", -1)
+	// Handle +/-
+	sign := 1
+	if str[0] == '+' || str[0] == '-' {
+		if str[0] == '-' {
+			sign = -1
+		}
+		str = str[1:]
+	}
+
+	n := 0
+	// Must be a number
+	for strbyte := range str {
+		if str[strbyte] < 48 || str[strbyte] > 57 {
+			return 0, errors.New("Error: Not Number.")
+		}
+		n = n*10 + int(str[strbyte]-'0')
+	}
+
+	return n * sign, nil
 }
---
+++ Part 1 String/1.5 Palindrome/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+}
+
+// Author: HackerZ
+// Time: 2016/06/10 23:23
+
+// isPalindrome
+// Input: madam  ||  我爱我  ||  StrToInt
+// Output: true  ||  true    ||  false
+
+// isPalindrome1
+// 解法1：两头往中间扫
+func isPalindrome1(str string, n int) bool {
+	// 非法输入
+	// if str == nil || n < 1 {
+	// 	return false
+	// }
+
+	return true
+}

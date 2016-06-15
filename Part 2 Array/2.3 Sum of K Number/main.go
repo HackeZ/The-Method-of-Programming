426f73f472e538def74fbe2503ee058fbcd4768a
HackeZ <767110505@qq.com>
2016-06-15 20:34:37 +0800

    add 2.3

---
+++ Part 2 Array/2.3 Sum of K Number/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	// Method 1
+
+}
+
+// Author: HackerZ
+// Time  : 2016/06/15 19:15
+
+// Sum of K Number
+// Input : 10  15 // n num
+// Output: 5+10 7+8
+
+// SumofkNumber1 n Problem turn to n-1 Problem
+// 解法1：n问题转换为n-1问题
+func SumofkNumber1(sum, n int) {
+
+}

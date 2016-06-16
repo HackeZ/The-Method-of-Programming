1629bd35f4e0a7daf6bf9ceba8d2dfd1ca8859d5
HackeZ <767110505@qq.com>
2016-06-16 13:46:47 +0800

    update 2.3

--- Part 2 Array/2.3 Sum of K Number/main.go
+++ Part 2 Array/2.3 Sum of K Number/main.go
@@ -1,8 +1,17 @@
 package main
 
+import (
+	"container/list"
+	"fmt"
+)
+
+var list1 = list.New()
+
 func main() {
 	// Method 1
-
+	fmt.Println("=== Method 1 ===")
+	SumofkNumber1(15, 8)
+	fmt.Println("=== Method 1 ===")
 }
 
 // Author: HackerZ
@@ -15,5 +24,20 @@ func main() {
 // SumofkNumber1 n Problem turn to n-1 Problem
 // 解法1：n问题转换为n-1问题
 func SumofkNumber1(sum, n int) {
+	// 递归出口
+	if n <= 0 || sum <= 0 {
+		return
+	}
+	// 输出找到的结果
+	if sum == n {
+		for iter := list1.Front(); iter != nil; iter = iter.Next() {
+			fmt.Printf("%v + ", iter.Value)
+		}
+		fmt.Printf("%d\n", n)
+	}
 
+	list1.PushFront(n)        // 典型的 01 背包问题
+	SumofkNumber1(sum-n, n-1) // 放n，前n-1个数填满sum-n
+	list1.Remove(list1.Front())
+	SumofkNumber1(sum, n-1) // 不放n，前n-1个数填满sum
 }

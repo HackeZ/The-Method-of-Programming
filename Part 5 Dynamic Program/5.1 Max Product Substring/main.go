fbb202e3a4af24e70bb89669f10788a8bcc9ee29
HackeZ <767110505@qq.com>
2016-06-26 15:15:50 +0800

    update 5.1

--- Part 5 Dynamic Program/5.1 Max Product Substring/main.go
+++ Part 5 Dynamic Program/5.1 Max Product Substring/main.go
@@ -10,6 +10,14 @@ func main() {
 	Input := []float64{-2.5, 4, 0, 3, 0.5, 8, -1}
 	fmt.Println("Output --> ", ViolentRoundRobin(Input, len(Input)))
 	fmt.Println("=== Method 1 ===")
+
+	fmt.Println("")
+
+	// Method 2
+	fmt.Println("=== Method 2 ===")
+	Input = []float64{-2.5, 4, 0, 3, 0.5, 8, -1}
+	fmt.Println("Output --> ", MaxProductSubstring(Input, len(Input)))
+	fmt.Println("=== Method 2 ===")
 }
 
 // Author: HackerZ
@@ -34,4 +42,35 @@ func ViolentRoundRobin(arrn []float64, length int) float64 {
 		}
 	}
 	return maxResult
+}
+
+// TIME:O(n^2)
+
+// MaxProductSubstring Max Product Substring
+// 解法2：动态规划
+func MaxProductSubstring(arrn []float64, length int) float64 {
+	maxEnd, minEnd, maxResult := arrn[0], arrn[0], arrn[0]
+	for i := 1; i < length; i++ {
+		end1, end2 := maxEnd*arrn[i], minEnd*arrn[i]
+		maxEnd = max(max(end1, end2), arrn[i])
+		minEnd = min(min(end1, end2), arrn[i])
+		maxResult = max(maxResult, maxEnd)
+	}
+	return maxResult
+}
+
+// TIME:O(n)
+
+func max(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
 }

827672a6e49ee6e39bd983ee4d79b12443f314cb
HackeZ <767110505@qq.com>
2016-07-09 16:21:59 +0800

    fix 5.3

---
+++ Part 5 Dynamic Program/5.3 Max Number of Squares/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+}
+
+// Author: HackerZ
+// Time  : 2016/06/27 19:47
+
+// Return the Max number by ergodic the N*N Squares twice.
+/*
+ * Input:   +-------------+
+ *          |0  0  0  0 13|
+ *          |21 0  0  4  0|
+ *          |0  0 14  0  0|
+ *          |0  15 0  0  0|
+ *          |0  0  0  0  0|
+ *          +-------------+
+ * Output:
+ */
---
+++ Part 5 Dynamic Program/5.3 Number of squares/main.go
@@ -0,0 +1,95 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Method 1
+	fmt.Println("=== Method 1 ===")
+	Input := [N][N]int{{0, 0, 3, 0, 2, 0}, {0, 0, 3, 0, 0, 0}, {0, 0, 3, 0, 0, 0}, {0, 0, 0, 0, 4, 0}, {0, 0, 0, 0, 4, 0}, {0, 0, 3, 0, 0, 0}}
+	Output := MinPathSum(Input, len(Input[0]))
+	fmt.Println("Output -->", Output)
+	fmt.Println("=== Method 1 ===")
+}
+
+// Author: HackerZ
+// Time  : 2016/06/27 19:47
+
+// Number of squares
+/*
+ * Input:   +----------------+
+ *          |0  0  3  0  2  0|
+ *          |0  0  3  0  0  0|
+ *          |0  0  3  0  0  0|
+ *          |0  0  0  0  4  0|
+ *          |0  0  0  0  4  0|
+ *          |0  0  3  0  0  0|
+ *          +----------------+
+ * Output:  22
+ */
+
+const (
+	N   = 202        // The Largest of
+	inf = 1000000000 // ∞
+)
+
+var dp [N * 2][N][N]int
+
+// IsValid : Is Status Valid
+func IsValid(step, x1, x2, n int) bool {
+	y1, y2 := step-x1, step-x2
+	return ((x1 >= 0) && (x1 < n) && (x2 >= 0) && (x2 < n) && (y1 >= 0) && (y1 < n) && (y2 >= 0) && (y2 < n))
+}
+
+// GetValue : Return Value of Dp.
+//            Return -∞ When Out of Array.
+func GetValue(step, x1, x2, n int) int {
+	if IsValid(step, x1, x2, n) {
+		return dp[step][x1][x2]
+	}
+	return (-inf)
+}
+
+// MinPathSum :
+func MinPathSum(a [N][N]int, n int) int {
+	P := n*2 - 2 // The End of Step
+	var i, j, step int
+
+	for i = 0; i < n; i++ {
+		for j = i; j < n; j++ {
+			dp[0][i][j] = -inf
+		}
+	}
+	dp[0][0][0] = a[0][0]
+
+	for step = 1; step <= P; step++ {
+		for i = 0; i < n; i++ {
+			for j = i; j < n; j++ {
+				dp[step][i][j] = -inf
+				if !IsValid(step, i, j, n) { // Not Valid
+					continue
+				}
+				// DFS for Valid Location.
+				if i != j {
+					dp[step][i][j] = max(dp[step][i][j], GetValue(step-1, i-1, j-1, n))
+					dp[step][i][j] = max(dp[step][i][j], GetValue(step-1, i-1, j, n))
+					dp[step][i][j] = max(dp[step][i][j], GetValue(step-1, i, j-1, n))
+					dp[step][i][j] = max(dp[step][i][j], GetValue(step-1, i, j, n))
+					dp[step][i][j] += a[i][step-i] + a[j][step-j] // In Different location, plus 2 number.
+				} else {
+					dp[step][i][j] = max(dp[step][i][j], GetValue(step-1, i-1, j-1, n))
+					dp[step][i][j] = max(dp[step][i][j], GetValue(step-1, i-1, j, n))
+					dp[step][i][j] = max(dp[step][i][j], GetValue(step-1, i, j-1, n))
+					dp[step][i][j] += a[i][step-i] // In Same location, plus 1 number.
+				}
+			}
+		}
+	}
+	return dp[P][n-1][n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}

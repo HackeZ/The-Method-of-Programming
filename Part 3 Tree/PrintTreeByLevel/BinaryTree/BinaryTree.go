c19797d1f433f1b5f0c602b7238a05f252f73eaa
HackeZ <767110505@qq.com>
2016-09-15 01:16:27 +0800

    update binarytree

--- Part 3 Tree/PrintTreeByLevel/BinaryTree/BinaryTree.go
+++ Part 3 Tree/PrintTreeByLevel/BinaryTree/BinaryTree.go
@@ -1,5 +1,7 @@
 package BinaryTree
 
+import "container/list"
+
 // BinaryTree ...
 type BinaryTree struct {
 	Root   *Node
@@ -66,4 +68,55 @@ func isEqual(value interface{}, node *Node) *Node {
 		}
 	}
 	return nil
+}
+
+// ProOrder return a List By ProOrder
+// @return *list.List
+func (bt *BinaryTree) ProOrder() *list.List {
+	traversal := list.New()
+	proOrder(bt.Root, traversal)
+	return traversal
+}
+
+func proOrder(n *Node, l *list.List) {
+	if n == nil {
+		return
+	}
+	l.PushBack(n)
+	proOrder(n.GetLeftNode(), l)
+	proOrder(n.GetRightNode(), l)
+}
+
+// InOrder return a List By InOrder
+// @return *list.List
+func (bt *BinaryTree) InOrder() *list.List {
+	traversal := list.New()
+	inOrder(bt.Root, traversal)
+	return traversal
+}
+
+func inOrder(n *Node, l *list.List) {
+	if n == nil {
+		return
+	}
+	inOrder(n.GetLeftNode(), l)
+	l.PushBack(n)
+	inOrder(n.GetRightNode(), l)
+}
+
+// PostOrder return a List By PostOrder
+// @return *list.List
+func (bt *BinaryTree) PostOrder() *list.List {
+	traversal := list.New()
+	postOrder(bt.Root, traversal)
+	return traversal
+}
+
+func postOrder(n *Node, l *list.List) {
+	if n == nil {
+		return
+	}
+	postOrder(n.GetLeftNode(), l)
+	postOrder(n.GetRightNode(), l)
+	l.PushBack(n)
 }
